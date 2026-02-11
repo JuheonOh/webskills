3c82a0832f7de8374a21f8f7a8f095f5aa7a8742
Randolph <dhwngjs01@naver.com>
2026-02-12 01:47:31 +0900

    fix: GitHub Pages 배포 설정 및 라우팅 이슈 수정
    - vite.config.js base 설정 추가 (/webskills/)
    
    - HashRouter로 전환 (GH Pages 호환성)
    
    - GitHub Actions 자동 배포 워크플로우 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
@@ -8,14 +8,14 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter
+      <HashRouter
         future={{
           v7_startTransition: true,
           v7_relativeSplatPath: true
         }}
       >
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </React.StrictMode>
 );
